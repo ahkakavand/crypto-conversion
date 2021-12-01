647b00dfaf940185f4eec25691172b9f73e57702
kakavand <kakavandah@gmail.com>
2021-12-01 18:14:47 +0330

    add function getNameCoin1 , removeOverlay , searchFirstCoin

--- js/main.js
+++ js/main.js
@@ -1,8 +1,51 @@
-let API_KEY = '2bdc4c1f-b626-45b1-8afa-c667c37c2f35'
+// variable
+let btnFirstSelect = document.querySelector('.first')
+let overlay = document.querySelector('.overlay')
+let searchBox = document.querySelector('.serchBox')
+let searchBox2 = document.querySelector('.serchBox2')
+let searchInput1 = document.querySelector('#search1')
 
-let name = (async () => {
-    url = "https://data.messari.io/api/v1/assets/btc/metrics"
-    let response = await fetch(url)
-    let notes = await response.json()
-    console.log(notes);
-})();
+
+// eventListeners
+btnFirstSelect.addEventListener('click' , searchFirstCoin)
+overlay.addEventListener('click' , removeOverlay)
+searchInput1.addEventListener('input' , getNameCoin1)
+
+
+
+// functions
+function searchFirstCoin(e) {
+    e.preventDefault()
+    searchBox.style.display = 'block'
+    overlay.style.display = 'block'
+}
+
+function removeOverlay(e) {
+    if (e.target.classList.contains('overlay')) {
+        overlay.style.display = 'none'
+        searchBox.style.display= 'none'
+        searchBox2.style.display= 'none'
+    }
+}
+
+function getNameCoin1(e) {
+    (async () => {
+        let url = "https://data.messari.io/api/v1/assets"
+        let response = await fetch(url)
+        let data = await response.json()
+        let allData = data.data
+        allData.forEach(item => {
+            for (let i = 0; i < e.target.value.length; i++) {
+                if (item.name.toLowerCase().includes(e.target.value)) {
+                    // let li = document.createElement('li')
+                    // li.innerText = item.name
+                    // searchBox.appendChild(li)
+                    
+                }
+            }
+        });
+
+
+    })();
+
+}
